7a47cf1a9825fc3651e919ad903d454226456115
1wsx10 <1wsx10@gmail.com>
2018-09-16 20:09:55 +0000

    On branch keyboard_input
    	modified:   main.c, main.h
    		added keyboard input and mainloop
    		the triangle changes shape when you press keys

--- main.c
+++ main.c
@@ -11,7 +11,8 @@ void drawTriangle() {
 		glBegin(GL_TRIANGLES);
 			glVertex3f(-0.7, 0.7, 0);
 			glVertex3f(0.7, 0.7, 0);
-			glVertex3f(0, -1, 0);
+			/*glVertex3f(0.0, -1.0, 0.0);*/
+			glVertex3f(corner[0], corner[1], corner[2]);
 		glEnd();
 
 	glFlush();
@@ -25,8 +26,91 @@ int main(int argc, char **argv) {
 	glutInitWindowPosition(100, 100);
 	glutCreateWindow("OpenGL - Creating a triangle");
 	glutDisplayFunc(drawTriangle);
+	glutKeyboardFunc(keyDown);
+	glutKeyboardUpFunc(keyUp);
+	glutIdleFunc(mainloop);
 	glutMainLoop();
 
 	return EXIT_SUCCESS;
 
 }
+
+void mainloop() {
+	
+	//vertical
+	do {
+		if(tDown && gDown) break;
+
+		if(tDown) {
+			corner[1] += 0.001f;
+		}
+		if(gDown) {
+			corner[1] -= 0.001f;
+		}
+	} while(0);
+	//horizontal
+	do {
+		if(hDown && fDown) break;
+
+		if(hDown) {
+			corner[0] += 0.001f;
+		}
+		if(fDown) {
+			corner[0] -= 0.001f;
+		}
+	} while(0);
+
+	glutPostRedisplay();
+}
+
+void keyDown(unsigned char key, int x, int y) {
+	switch(key) {
+		//quit
+		case 'Q':
+		case 'q':
+			glutLeaveMainLoop();
+			break;
+
+		//arrow keys
+		case 't':
+		case 'T':
+			tDown = 1;
+			break;
+		case 'g':
+		case 'G':
+			gDown = 1;
+			break;
+		case 'f':
+		case 'F':
+			fDown = 1;
+			break;
+		case 'h':
+		case 'H':
+			hDown = 1;
+			break;
+	}
+}
+
+void keyUp(unsigned char key, int x, int y) {
+	switch(key) {
+
+		//arrow keys
+		case 't':
+		case 'T':
+			tDown = 0;
+			break;
+		case 'g':
+		case 'G':
+			gDown = 0;
+			break;
+		case 'f':
+		case 'F':
+			fDown = 0;
+			break;
+		case 'h':
+		case 'H':
+			hDown = 0;
+			break;
+	}
+}
+
--- main.h
+++ main.h
@@ -7,7 +7,18 @@
 #include <GL/freeglut.h>
 #include <GL/gl.h>
 
+int tDown = 0;
+int gDown = 0;
+int fDown = 0;
+int hDown = 0;
+
+//x, y, z coordinate of one of the triangle corners
+float corner[] = {0.0f, -0.5f, 0.0f};
+
 // from opengl hello world tutorial
 void drawTriangle();
+void keyDown(unsigned char key, int x, int y);
+void keyUp(unsigned char key, int x, int y);
+void mainloop();
 
 int main();
