e54cfe8c123a3f0450fd7e60cdd49d904cecdfb1
1wsx10 <1wsx10@gmail.com>
2018-09-16 18:16:57 +0000

    starting with an opengl hello world program

---
+++ main.h
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <stdio.h>
+/*#include <curses.h>*/
+#include <math.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <GL/freeglut.h>
+#include <GL/gl.h>
+
+// from opengl hello world tutorial
+void drawTriangle();
+
+int main();
