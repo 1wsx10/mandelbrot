de398b5e1b702ef3620d290171692bd696835189
1wsx10 <1wsx10@gmail.com>
2019-01-30 12:15:40 +1100

    added worker pool behavior, so that the faster threads can work on the remaining portion of the image, rather than waiting for the slower threads. also made it stop drawing after an image is finished, so that it doesn't continue heating your cpu.

--- main.h
+++ main.h
@@ -20,12 +20,57 @@
 #include <curses.h>
 
 typedef struct mandle_controls {
+	long int current_frame;
 	char is_running;
 	int depth;
 	long double zoom;
 	long double R;
 	long double I;
 } MANDLE_CONTROLS;
+
+struct tdraw_data {
+	pthread_t tid;
+	MANDLE_CONTROLS *cont;
+};
+
+//threads:
+// 1: mark as idle
+// 2: wait to be told to work
+// (loop):
+//	3: take a job slot
+//		or break to go back to 1 if no jobs
+//	4: set as working
+//	== do the work ==
+//	5: set as finished
+
+/* signals to the controller that the thread is waiting */
+pthread_mutex_t currently_idle_mutex = PTHREAD_MUTEX_INITIALIZER;
+pthread_cond_t  currently_idle_cond  = PTHREAD_COND_INITIALIZER;
+int currently_idle;
+
+/* signals to the thread they can take a job */
+pthread_mutex_t work_ready_mutex = PTHREAD_MUTEX_INITIALIZER;
+pthread_cond_t work_ready_cond = PTHREAD_COND_INITIALIZER;
+char work_ready;
+
+/* protects the work_done value */
+pthread_mutex_t work_done_mutex = PTHREAD_MUTEX_INITIALIZER;
+int work_done;
+int work_length;
+int work_count;
+
+pthread_cond_t  currently_working_cond = PTHREAD_COND_INITIALIZER;
+pthread_mutex_t currently_working_mutex= PTHREAD_MUTEX_INITIALIZER;
+int currently_working;
+
+/**
+ * makes a RGBT value out of a number (0-255)
+ */
+void make_colour(int val, int depth, RGBT *ret);
+
+#ifndef PI
+#define PI 3.14159f
+#endif
 
 #ifndef MIN
 #define MIN(X,Y) ((X) < (Y) ? (X) : (Y))
